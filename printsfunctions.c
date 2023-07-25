a72b59f95845ab8def6a0795b5984b9b10772a2c
Sammy5049 <104107018+Sammy5049@users.noreply.github.com>
2023-07-25 12:34:01 +0100

    Update printsfunctions.c  focus

--- printsfunctions.c
+++ printsfunctions.c
@@ -33,7 +33,7 @@ int print_char(va_list types, char buffer[],
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int length = 0, i;
+	int length = 0, counter;
 	char *str = va_arg(types, char *);
 
 	UNUSED(buffer);
@@ -59,13 +59,13 @@ int print_string(va_list types, char buffer[],
 		if (flags & F_MINUS)
 		{
 			write(1, &str[0], length);
-			for (i = width - length; i > 0; i--)
+			for (counter = width - length; counter > 0; counter--)
 				write(1, " ", 1);
 			return (width);
 		}
 		else
 		{
-			for (i = width - length; i > 0; i--)
+			for (counter = width - length; counter > 0; counter--)
 				write(1, " ", 1);
 			write(1, &str[0], length);
 			return (width);
@@ -155,9 +155,9 @@ int print_int(va_list types, char buffer[],
 int print_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	unsigned int n, m, i, sum;
+	unsigned int n, m, counter, sum;
 	unsigned int a[32];
-	int count;
+	int totalcount;
 
 	UNUSED(buffer);
 	UNUSED(flags);
@@ -168,22 +168,22 @@ int print_binary(va_list types, char buffer[],
 	n = va_arg(types, unsigned int);
 	m = 2147483648; /* (2 ^ 31) */
 	a[0] = n / m;
-	for (i = 1; i < 32; i++)
+	for (counter = 1; counter < 32; counter++)
 	{
 		m /= 2;
-		a[i] = (n / m) % 2;
+		a[counter] = (n / m) % 2;
 	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	for (counter = 0, sum = 0, totalcount = 0; counter < 32; counter++)
 	{
-		sum += a[i];
-		if (sum || i == 31)
+		sum += a[counter];
+		if (sum || counter == 31)
 		{
-			char z = '0' + a[i];
+			char z = '0' + a[counter];
 
 			write(1, &z, 1);
-			count++;
+			totalcount++;
 		}
 	}
-	return (count);
+	return (totalcount);
 }
 
